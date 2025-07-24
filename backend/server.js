ce4ec39c476aa8b66f601db64cb4ed1799f2df8c
Victor Seleimend <sseleimend@gmail.com>
2025-07-24 12:22:49 -0300

    Update server with dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-import e from "express";
+import express from "express";
+import dotenv from "dotenv";
 
-const app = e();
+dotenv.config();
+
+const app = express();
+
+app.get("/products", (req, res) => {});
 
 app.listen(3000, () => {
   console.log(`Server started at http://localhost:3000`);
