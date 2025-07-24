682e8ca12ea093aec9cab407570ce73e5ccb11d5
Victor Seleimend <sseleimend@gmail.com>
2025-07-24 13:36:26 -0300

    Reorganize imports

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
 
+import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
