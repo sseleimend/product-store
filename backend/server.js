4902b6cc584fd89677a687337d3300cdf527fd49
Victor Seleimend <sseleimend@gmail.com>
2025-07-24 12:32:45 -0300

    Add db connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -8,5 +9,6 @@ const app = express();
 app.get("/products", (req, res) => {});
 
 app.listen(3000, () => {
+  connectDB();
   console.log(`Server started at http://localhost:3000`);
 });
