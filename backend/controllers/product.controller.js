eeee64cecd711d107afce50617e24f05d9cd2a9e
Victor Seleimend <sseleimend@gmail.com>
2025-07-24 13:38:13 -0300

    Fix product.model.js import path

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-import Product from "./models/product.model.js";
+import Product from "../models/product.model.js";
 
 export const getProducts = async (req, res) => {
   try {
