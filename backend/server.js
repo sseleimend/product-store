660e5f6ee6b0c6bb7cebdda47256427331533d24
Victor Seleimend <sseleimend@gmail.com>
2025-07-25 10:12:50 -0300

    Move port number to an env var

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,15 @@ import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT ?? 3000;
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log(`Server started at http://localhost:3000`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
