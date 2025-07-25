559e5cdb5853b348939c7c410519e900aa6ba028
Victor Seleimend <sseleimend@gmail.com>
2025-07-25 13:16:07 -0300

    Add product store

---
+++ frontend/src/store/product.ts
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+
+interface Product {
+  name: string;
+  price: number;
+  image: string;
+}
+
+interface ProductStore {
+  products: Product[];
+  setProducts: (products: Product[]) => void;
+  createProduct: (newProduct: Product) => Promise<{
+    success: boolean;
+    message: string;
+  }>;
+}
+
+export const useProductStore = create<ProductStore>((set) => ({
+  products: [],
+  setProducts: (products: Product[]) => set({ products }),
+  createProduct: async (newProduct: Product) => {
+    if (!newProduct.name || !newProduct.image || !newProduct.price) {
+      return { success: false, message: "Please fill in all fields" };
+    }
+    const res = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    });
+    const data = await res.json();
+    set((state) => ({
+      products: [...state.products, data.data],
+    }));
+    return { success: true, message: "Product created successfully" };
+  },
+}));
