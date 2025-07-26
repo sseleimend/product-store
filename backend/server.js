7fe08022b54ba34acef94cb8d7c4bb5246e7aef3
Victor Seleimend <sseleimend@gmail.com>
2025-07-26 10:12:24 -0300

    Fix path to regex error

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api/products", productRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get((req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
