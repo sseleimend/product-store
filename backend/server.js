b9a74700367442275b43fa9eedb9c39570f41f2e
Victor Seleimend <sseleimend@gmail.com>
2025-07-26 09:54:21 -0300

    Add build script

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
@@ -8,11 +9,21 @@ dotenv.config();
 
 const PORT = process.env.PORT ?? 3000;
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
